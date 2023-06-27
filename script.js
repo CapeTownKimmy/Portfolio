a4e37fe45579e99c956217d0b7dd4e2084a46c6b
CapeTownKimmy <122791435+CapeTownKimmy@users.noreply.github.com>
2023-06-27 13:39:41 +0100

    Edit cardContainer, Add projects

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 const cards = document.querySelectorAll('.card'); //Array of Cards//
 const cardText = document.querySelectorAll('h2');
 
-cardText.forEach(element => console.log(element))
+cardText.forEach(element => {
+if(element.innerHTML === ''){
+    element.innerHTML = 'Coming Soon...';
+    element.style.color = '#949fa6';
+    element.style.backgroundColor = '#b4bbbf'
+    element.style.boxShadow = 'none';
+    let cardBorder = element.parentElement.parentElement;
+    cardBorder.style.border = 'none';
+}
+})
