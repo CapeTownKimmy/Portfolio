685258d2860e1ec586e9ce52e64ee674fd0e0f65
CapeTownKimmy <122791435+CapeTownKimmy@users.noreply.github.com>
2023-06-28 17:01:05 +0100

    Add basic contact Pop Up

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+// Card Display //
 const cards = document.querySelectorAll('.card'); //Array of Cards//
-const cardText = document.querySelectorAll('h2');
+const cardText = document.querySelectorAll('.cardName');
 
+// Contact Pop Up //
+const contactBtn = document.getElementById('contact');
+const overlay = document.getElementById('overlay');
+const popUp = document.getElementById('popUpContainer');
+const closePopUpBtn = document.getElementById('closeBtn');
+
+
+// Card Display - Functions //
 cardText.forEach(element => {
 if(element.innerHTML === ''){
     element.innerHTML = 'Coming Soon...';
@@ -11,3 +20,20 @@ if(element.innerHTML === ''){
     cardBorder.style.border = 'none';
 }
 })
+
+// Contact Pop Up - Events and Functions //
+contactBtn.addEventListener('click', showPopUp); // Show Pop Up on Click//
+closePopUpBtn.addEventListener('click', closePopUp); // Close on Click X //
+overlay.addEventListener('click', closePopUp); // Close on overlay click //
+
+function showPopUp(event) {
+    overlay.hidden = false;
+    popUp.style.visibility = 'visible';
+    popUp.style.opacity = '1';
+}
+function closePopUp(event) {
+    overlay.hidden = true;
+    popUp.style.opacity = '0';
+    popUp.style.visibility = 'hidden';
+}
+
