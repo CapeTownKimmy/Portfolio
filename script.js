673d2064b4d5e5f9de7ab23d791d453a97f886d0
CapeTownKimmy <122791435+CapeTownKimmy@users.noreply.github.com>
2023-06-29 12:41:45 +0100

    Edit contact page, Add background pattern

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const cards = document.querySelectorAll('.card'); //Array of Cards//
 const cardText = document.querySelectorAll('.cardName');
 
 // Contact Pop Up //
-const contactBtn = document.getElementById('contact');
-const overlay = document.getElementById('overlay');
-const popUp = document.getElementById('popUpContainer');
-const closePopUpBtn = document.getElementById('closeBtn');
+// const contactBtnIndex = document.getElementById('contactIndex');
+// const overlay = document.getElementById('overlay');
+// const popUp = document.getElementById('popUpContainer');
+// const closePopUpBtn = document.getElementById('closeBtn');
 
 
 // Card Display - Functions //
@@ -22,18 +22,18 @@ if(element.innerHTML === ''){
 })
 
 // Contact Pop Up - Events and Functions //
-contactBtn.addEventListener('click', showPopUp); // Show Pop Up on Click//
-closePopUpBtn.addEventListener('click', closePopUp); // Close on Click X //
-overlay.addEventListener('click', closePopUp); // Close on overlay click //
+// contactBtnIndex.addEventListener('click', showPopUp); // Show Pop Up on Click - Index Page //
+// closePopUpBtn.addEventListener('click', closePopUp); // Close on Click X //
+// overlay.addEventListener('click', closePopUp); // Close on overlay click //
 
 function showPopUp(event) {
     overlay.hidden = false;
     popUp.style.visibility = 'visible';
     popUp.style.opacity = '1';
+    console.log(event.target);
 }
 function closePopUp(event) {
     overlay.hidden = true;
     popUp.style.opacity = '0';
     popUp.style.visibility = 'hidden';
 }
-
